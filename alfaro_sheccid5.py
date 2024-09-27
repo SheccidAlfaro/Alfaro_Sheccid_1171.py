c2597debb8feb440cbd84cbad633a82efe30c614
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-27 17:53:12 -0600

    Add files via upload

---
+++ alfaro_sheccid5.py
@@ -0,0 +1,15 @@
+print("")
+print("Sheccid Alfaro Ibarra 3w, 11721 numeros naturales")
+#SE define y se pregunta al usuario
+nmt=int(input("Ingrese un numero natural:"))
+#Se utiliza if para saner si es numero natural y si es impar
+if nmt <=12:
+    print("Esta entre los numeros naturales.")
+else:
+    print("No esta entre los numeros naturales.")
+    
+if nmt % 2 == 0 :
+    print("es par")#una de las condiciones
+#en caso de que la primera opcion sea falsa
+else:
+    print("es impar")#una de las condiciones
