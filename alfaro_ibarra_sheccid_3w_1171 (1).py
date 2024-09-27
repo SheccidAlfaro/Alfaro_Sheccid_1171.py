9998d273769ac64773f07f8c5fa0560308f7d51c
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-27 17:32:30 -0600

    Add files via upload

---
+++ alfaro_ibarra_sheccid_3w_1171 (1).py
@@ -0,0 +1,13 @@
+print("")
+#DEfinir variables y preguntar al usuario
+print("Alfaro Ibarra Sheccid 3W 1171 hayar el perimetro y area de un rectangulo")
+b1=float(input("Incerta el numero de la base de ancho"))
+al=float(input("Incerta la altura"))
+#definir variable y hacer operaciones
+p=b1 + b1 + al + al
+h=b1*al
+#Mostrar en pantalla
+print("El perimetro es:")
+print(p)
+print("EL area es:")
+print(h)
